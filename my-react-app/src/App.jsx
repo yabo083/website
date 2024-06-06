942ff0e3d0103099b8279e78d6ae22803a2b44cf
yabo083 <103875886+yabo083@users.noreply.github.com>
2024-06-06 11:38:39 +0800

    init

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card";
+
+function App() {
+    return (
+        <>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        </>
+    )
+}
+
+export default App;
