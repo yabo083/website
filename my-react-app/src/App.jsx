9029a37d1479e5dd03b9767fb88778f7c690b89b
yabo083 <103875886+yabo083@users.noreply.github.com>
2024-06-11 17:58:38 +0800

    创建对象数组与更新

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import MyComponent from "./MyComponent";
 
 function App() {
   return (
-    <div className="flex flex-col justify-center items-center mt-5 ">
+    <div className="flex flex-col">
       <MyComponent />
     </div>
   );
--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -2,34 +2,68 @@ import { useState } from "react";
 import React from "react";
 
 const MyComponent = () => {
-  const [foods, setFoods] = useState(["Apple", "Orange", "Banana"]);
+  const [cars, setCars] = useState([]);
+  const [carYear, setCarYear] = useState(new Date().getFullYear());
+  const [carMake, setCarMake] = useState("");
+  const [carModel, setCarModel] = useState("");
 
-  const handleAddFood = () => {
-    const newFood = document.getElementById("foodInput").value;
-    document.getElementById("foodInput").value = "";
-    setFoods((f) => [...f, newFood]);
+  const handleAddCar = () => {
+    setCars((prevCars) => [
+      ...prevCars,
+      {
+        year: carYear,
+        make: carMake,
+        model: carModel,
+      },
+    ]);
+    setCarYear(new Date().getFullYear());
+    setCarMake("");
+    setCarModel("");
   };
 
-  const handleRemoveFood = (index) => {
-    // const newFoods = [...foods];
-    // newFoods.splice(index, 1);
-    // setFoods(newFoods);
+  const hanldeRemoveCar = (index) => {
+    setCars((prevCars) => prevCars.filter((_, i) => i !== index));
+  };
 
-    setFoods(f => f.filter((_, i) => i !== index)) // better!
+  const handleMakeChange = (e) => {
+    setCarMake(e.target.value);
+  };
+
+  const handleModelChange = (e) => {
+    setCarModel(e.target.value);
+  };
+
+  const handleYearChange = (e) => {
+    setCarYear(e.target.value);
   };
 
   return (
-    <div>
-      <h2>List of Food</h2>
+    <div className="flex flex-col justify-center items-center">
+      <h2>List of Car Objects</h2>
       <ul>
-        {foods.map((food, index) => (
-          <li className="cursor-pointer" key={index} onClick={() => handleRemoveFood(index)}>
-            {food}
+        {cars.map((car, index) => (
+          <li key={index} onClick={() => hanldeRemoveCar(index)}>
+            {car.year} {car.make} {car.model}{" "}
           </li>
         ))}
       </ul>
-      <input type="text" id="foodInput" placeholder="Enter food name" />
-      <button onClick={handleAddFood}>Add Food</button>
+      <input type="number" value={carYear} onChange={handleYearChange} />
+      <br />
+      <input
+        type="text"
+        value={carMake}
+        onChange={handleMakeChange}
+        placeholder="Enter car make"
+      />
+      <br />
+      <input
+        type="text"
+        value={carModel}
+        onChange={handleModelChange}
+        placeholder="Enter car model"
+      />
+      <br />
+      <button onClick={handleAddCar}>Add Car</button>
     </div>
   );
 };
